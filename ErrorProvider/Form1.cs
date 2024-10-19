c021cc2c2a0d691edbea649b0585cbe66e202c06
Amin Hasanov <44285676+aminhesenov@users.noreply.github.com>
2024-10-19 14:57:47 +0400

    ErrorProvider
    Sign in page

---
+++ ErrorProvider/Form1.cs
@@ -0,0 +1,32 @@
+﻿namespace ErrorProvider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == string.Empty)
+            {
+                errorProvider1.SetError(textBox1, "Username empty");
+            }
+            else
+            {
+                errorProvider1.Clear();
+
+            }
+            if(textBox2.Text == string.Empty)
+            {
+                errorProvider2.SetError(textBox2, "Password empty");
+            }
+            else
+            {
+                errorProvider2.Clear();
+            }
+
+        }
+    }
+}
